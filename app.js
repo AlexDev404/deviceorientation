c51c22132fcab293730aca962ab658baf6b22059
AlexDev404 <alexdev404@users.noreply.github.com>
2022-08-10 21:41:17 -0600

    Update Client Script

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ if ("WebSocket" in window) {
 }
 
 function handleOrientation(event) {
-  updateFieldIfNotNull("Orientation_a", event.alpha);
-  updateFieldIfNotNull("Orientation_b", event.beta);
-  updateFieldIfNotNull("Orientation_g", event.gamma);
+  // updateFieldIfNotNull("Orientation_a", event.alpha);
+  // updateFieldIfNotNull("Orientation_b", event.beta);
+  // updateFieldIfNotNull("Orientation_g", event.gamma);
   incrementEventCount();
   // sendToServer({ x: event.beta, y: event.gamma, z: event.alpha }, "ws://192.168.199.149:8010/");
   if (wsOpen) {
@@ -34,37 +34,33 @@ function updateFieldIfNotNull(fieldName, value, precision = 10) {
 }
 
 function handleMotion(event) {
-  updateFieldIfNotNull(
-    "Accelerometer_gx",
-    event.accelerationIncludingGravity.x
-  );
-  updateFieldIfNotNull(
-    "Accelerometer_gy",
-    event.accelerationIncludingGravity.y
-  );
-  updateFieldIfNotNull(
-    "Accelerometer_gz",
-    event.accelerationIncludingGravity.z
-  );
+  // updateFieldIfNotNull(
+  //   "Accelerometer_gx",
+  //   event.accelerationIncludingGravity.x
+  // );
+  // updateFieldIfNotNull(
+  //   "Accelerometer_gy",
+  //   event.accelerationIncludingGravity.y
+  // );
+  // updateFieldIfNotNull(
+  //   "Accelerometer_gz",
+  //   event.accelerationIncludingGravity.z
+  // );
 
-  updateFieldIfNotNull("Accelerometer_x", event.acceleration.x);
-  updateFieldIfNotNull("Accelerometer_y", event.acceleration.y);
-  updateFieldIfNotNull("Accelerometer_z", event.acceleration.z);
+  // updateFieldIfNotNull("Accelerometer_x", event.acceleration.x);
+  // updateFieldIfNotNull("Accelerometer_y", event.acceleration.y);
+  // updateFieldIfNotNull("Accelerometer_z", event.acceleration.z);
 
-  updateFieldIfNotNull("Accelerometer_i", event.interval, 2);
+  // updateFieldIfNotNull("Accelerometer_i", event.interval, 2);
 
-  updateFieldIfNotNull("Gyroscope_z", event.rotationRate.alpha);
-  updateFieldIfNotNull("Gyroscope_x", event.rotationRate.beta);
-  updateFieldIfNotNull("Gyroscope_y", event.rotationRate.gamma);
+  // updateFieldIfNotNull("Gyroscope_z", event.rotationRate.alpha);
+  // updateFieldIfNotNull("Gyroscope_x", event.rotationRate.beta);
+  // updateFieldIfNotNull("Gyroscope_y", event.rotationRate.gamma);
   incrementEventCount();
 }
 
 let is_running = false;
-let demo_button = document.getElementById("start_demo");
-
-demo_button.onclick = function (e) {
-  e.preventDefault();
-
+((e) => {
   // Request permission for iOS 13+ devices
   if (
     typeof DeviceMotionEvent != "undefined" &&
@@ -76,16 +72,10 @@ demo_button.onclick = function (e) {
   if (is_running) {
     window.removeEventListener("devicemotion", handleMotion);
     window.removeEventListener("deviceorientation", handleOrientation);
-    demo_button.innerHTML = "Start demo";
-    demo_button.classList.add("btn-success");
-    demo_button.classList.remove("btn-danger");
     is_running = false;
   } else {
     window.addEventListener("devicemotion", handleMotion);
     window.addEventListener("deviceorientation", handleOrientation);
-    document.getElementById("start_demo").innerHTML = "Stop demo";
-    demo_button.classList.remove("btn-success");
-    demo_button.classList.add("btn-danger");
     is_running = true;
   }
-};
+})();
