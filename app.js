d6f7c18d73870c65166c8416e8892aa1e4a462d5
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-08-10 22:58:08 -0600

    Switch + avoid errors

--- app.js
+++ app.js
@@ -90,11 +90,11 @@ let is_running = false;
 
 
 (function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
+  if (document.body.requestFullscreen) {
+    document.body.requestFullscreen();
+  } else if (document.body.webkitRequestFullscreen) { /* Safari */
+    document.body.webkitRequestFullscreen();
+  } else if (document.body.msRequestFullscreen) { /* IE11 */
+    document.body.msRequestFullscreen();
   }
 })();
